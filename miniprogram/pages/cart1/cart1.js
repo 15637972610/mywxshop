32bd4b184dcc4f846cdc7f8efbe8c0bc1a4e6acf
dkp17801090519 <dukunpeng@chinasofti.com>
2019-01-09 18:45:54 +0800

    添加购物车部分逻辑

--- miniprogram/pages/cart1/cart1.js
+++ miniprogram/pages/cart1/cart1.js
@@ -1,11 +1,15 @@
-// miniprogram/pages/cart1/cart1.js
+// pages/cart1/cart1.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    carts: [], //数据 
+    iscart: false,
+    hidden: null,
+    isAllSelect: false,
+    totalMoney: 0,
   },
 
   /**
@@ -14,53 +18,163 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    // 获取产品展示页保存的缓存数据（购物车的缓存数组，没有数据，则赋予一个空数组）  
+    var arr = wx.getStorageSync('cart') || [];
+    console.info("缓存数据：" + arr);
+    // 有数据的话，就遍历数据，计算总金额 和 总数量  
+    if (arr.length > 0) {
+      // 更新数据  
+      this.setData({
+        carts: arr,
+        iscart: true,
+        hidden: false
+      });
+      console.info("缓存数据：" + this.data.carts);
+    } else {
+      this.setData({
+        iscart: false,
+        hidden: true,
+      });
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  //勾选事件处理函数  
+  switchSelect: function (e) {
+    // 获取item项的id，和数组的下标值  
+    var Allprice = 0, i = 0;
+    let id = e.target.dataset.id,
 
+      index = parseInt(e.target.dataset.index);
+    this.data.carts[index].isSelect = !this.data.carts[index].isSelect;
+    //价钱统计
+    if (this.data.carts[index].isSelect) {
+      this.data.totalMoney = this.data.totalMoney + (this.data.carts[index].price * this.data.carts[index].count);
+    }
+    else {
+      this.data.totalMoney = this.data.totalMoney - (this.data.carts[index].price * this.data.carts[index].count);
+    }
+    //是否全选判断
+    for (i = 0; i < this.data.carts.length; i++) {
+      Allprice = Allprice + (this.data.carts[index].price * this.data.carts[index].count);
+    }
+    if (Allprice == this.data.totalMoney) {
+      this.data.isAllSelect = true;
+    }
+    else {
+      this.data.isAllSelect = false;
+    }
+    this.setData({
+      carts: this.data.carts,
+      totalMoney: this.data.totalMoney,
+      isAllSelect: this.data.isAllSelect,
+    })
   },
+  //全选
+  allSelect: function (e) {
+    //处理全选逻辑
+    let i = 0;
+    if (!this.data.isAllSelect) {
+      this.data.totalMoney = 0;
+      for (i = 0; i < this.data.carts.length; i++) {
+        this.data.carts[i].isSelect = true;
+        this.data.totalMoney = this.data.totalMoney + (this.data.carts[i].price * this.data.carts[i].count);
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+      }
+    }
+    else {
+      for (i = 0; i < this.data.carts.length; i++) {
+        this.data.carts[i].isSelect = false;
+      }
+      this.data.totalMoney = 0;
+    }
+    this.setData({
+      carts: this.data.carts,
+      isAllSelect: !this.data.isAllSelect,
+      totalMoney: this.data.totalMoney,
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  // 去结算
+  toBuy() {
+    wx.showToast({
+      title: '去结算',
+      icon: 'success',
+      duration: 3000
+    });
+    this.setData({
+      showDialog: !this.data.showDialog
+    });
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  //数量变化处理
+  handleQuantityChange(e) {
+    var componentId = e.componentId;
+    var quantity = e.quantity;
+    this.data.carts[componentId].count.quantity = quantity;
+    this.setData({
+      carts: this.data.carts,
+    });
   },
+  /* 减数 */
+  delCount: function (e) {
+    var index = e.target.dataset.index;
+    console.log("刚刚您点击了加一");
+    var count = this.data.carts[index].count;
+    // 商品总数量-1
+    if (count > 1) {
+      this.data.carts[index].count--;
+    }
+    // 将数值与状态写回  
+    this.setData({
+      carts: this.data.carts
+    });
+    console.log("carts:" + this.data.carts);
+    this.priceCount();
+  },
+  /* 加数 */
+  addCount: function (e) {
+    var index = e.target.dataset.index;
+    console.log("刚刚您点击了加+");
+    var count = this.data.carts[index].count;
+    // 商品总数量+1  
+    if (count < 10) {
+      this.data.carts[index].count++;
+    }
+    // 将数值与状态写回  
+    this.setData({
+      carts: this.data.carts
+    });
+    console.log("carts:" + this.data.carts);
+    this.priceCount();
+  },
+  priceCount: function (e) {
+    this.data.totalMoney = 0;
+    for (var i = 0; i < this.data.carts.length; i++) {
+      if (this.data.carts[i].isSelect == true) {
+        this.data.totalMoney = this.data.totalMoney + (this.data.carts[i].price * this.data.carts[i].count);
+      }
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    }
+    this.setData({
+      totalMoney: this.data.totalMoney,
+    })
+  },
+  /* 删除item */
+  delGoods: function (e) {
+    this.data.carts.splice(e.target.id.substring(3), 1);
+    // 更新data数据对象  
+    if (this.data.carts.length > 0) {
+      this.setData({
+        carts: this.data.carts
+      })
+      wx.setStorageSync('cart', this.data.carts);
+      this.priceCount();
+    } else {
+      this.setData({
+        cart: this.data.carts,
+        iscart: false,
+        hidden: true,
+      })
+      wx.setStorageSync('cart', []);
+    }
   }
 })
